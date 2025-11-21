0be3027e77a2a19f37c5170b6704f450ef4a7ee5
Skele <abdelrahmanebed@gmail.com>
2025-11-21 12:13:43 -0500

    Changed CMake to work with mint instead

---
+++ build/CMakeFiles/GravitySimulator.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/media/skele/OS/Shared/Documents/ASTUFFA/vscode/!Serious/gravity-simulator/Learning/SDLPractice/Core/EvenManager.cpp" "CMakeFiles/GravitySimulator.dir/Learning/SDLPractice/Core/EvenManager.cpp.o" "gcc" "CMakeFiles/GravitySimulator.dir/Learning/SDLPractice/Core/EvenManager.cpp.o.d"
+  "/media/skele/OS/Shared/Documents/ASTUFFA/vscode/!Serious/gravity-simulator/Learning/SDLPractice/Core/Initializer.cpp" "CMakeFiles/GravitySimulator.dir/Learning/SDLPractice/Core/Initializer.cpp.o" "gcc" "CMakeFiles/GravitySimulator.dir/Learning/SDLPractice/Core/Initializer.cpp.o.d"
+  "/media/skele/OS/Shared/Documents/ASTUFFA/vscode/!Serious/gravity-simulator/Learning/SDLPractice/Core/Window.cpp" "CMakeFiles/GravitySimulator.dir/Learning/SDLPractice/Core/Window.cpp.o" "gcc" "CMakeFiles/GravitySimulator.dir/Learning/SDLPractice/Core/Window.cpp.o.d"
+  "/media/skele/OS/Shared/Documents/ASTUFFA/vscode/!Serious/gravity-simulator/Learning/SDLPractice/main.cpp" "CMakeFiles/GravitySimulator.dir/Learning/SDLPractice/main.cpp.o" "gcc" "CMakeFiles/GravitySimulator.dir/Learning/SDLPractice/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/GravitySimulator.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GravitySimulator.dir/Learning/SDLPractice/Core/EvenManager.cpp.o"
+  "CMakeFiles/GravitySimulator.dir/Learning/SDLPractice/Core/EvenManager.cpp.o.d"
+  "CMakeFiles/GravitySimulator.dir/Learning/SDLPractice/Core/Initializer.cpp.o"
+  "CMakeFiles/GravitySimulator.dir/Learning/SDLPractice/Core/Initializer.cpp.o.d"
+  "CMakeFiles/GravitySimulator.dir/Learning/SDLPractice/Core/Window.cpp.o"
+  "CMakeFiles/GravitySimulator.dir/Learning/SDLPractice/Core/Window.cpp.o.d"
+  "CMakeFiles/GravitySimulator.dir/Learning/SDLPractice/main.cpp.o"
+  "CMakeFiles/GravitySimulator.dir/Learning/SDLPractice/main.cpp.o.d"
+  "GravitySimulator"
+  "GravitySimulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GravitySimulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
